8472ac29639c03a668b2759d4c816e2468a1b7a3
Laurel Sun <86388357+sunlaurel@users.noreply.github.com>
2025-08-05 09:14:07 -0500

    need to finish later

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Laurel Sun</h1>
+      <p>Hi! 👋</p>
+      <p>I'm Laurel, a CS major at the <a href="https://www.cs.umd.edu/" target='_blank'>
+        University of Maryland</a>.
+        I'm broadly interested in the field of computer vision and robotics.
+        I'm also interested in software engineering and big data processing.
+        Feel free to reach out!</p>
+      <div className='contact'>
+        <p>✉️  laurel.sun.06@gmail.com</p>
+        <p>🌐  lsun2006@umd.edu</p>
+        <p>🔗  https://www.linkedin.com/in/laurel-sun/</p>
+        <p>💻  https://github.com/sunlaurel/</p>
+      </div>
     </div>
+
   );
 }
 
